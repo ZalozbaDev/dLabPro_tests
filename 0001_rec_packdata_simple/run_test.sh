73359c840da7bd672b7e8afa73bdc4dc4ba18ca8
Daniel Zoba <daniel.sobe@sorben.com>
2021-08-17 13:52:16 +0200

    rearrange commands for verification

--- 0001_rec_packdata_simple/run_test.sh
+++ 0001_rec_packdata_simple/run_test.sh
@@ -17,15 +17,16 @@ mkdir tmpcompare
 
 zcat expected/feainfo.object > tmpcompare/feainfo.object.expected.txt
 zcat generated/feainfo.object > tmpcompare/feainfo.object.generated.txt
-diff tmpcompare/feainfo.object.expected.txt tmpcompare/feainfo.object.generated.txt
 
 zcat expected/sesinfo.object > tmpcompare/sesinfo.object.expected.txt
 zcat generated/sesinfo.object > tmpcompare/sesinfo.object.generated.txt
-diff tmpcompare/sesinfo.object.expected.txt tmpcompare/sesinfo.object.generated.txt
 
 zcat expected/adapted/3_20_hsb_adp.gmm > tmpcompare/3_20_hsb_adp.gmm.expected.txt
 zcat generated/adapted/3_20_hsb_adp.gmm > tmpcompare/3_20_hsb_adp.gmm.generated.txt
-diff tmpcompare/3_20_hsb_adp.gmm.expected.txt tmpcompare/3_20_hsb_adp.gmm.generated.txt
+
+diff -q tmpcompare/feainfo.object.expected.txt tmpcompare/feainfo.object.generated.txt
+diff -q tmpcompare/sesinfo.object.expected.txt tmpcompare/sesinfo.object.generated.txt
+diff -q tmpcompare/3_20_hsb_adp.gmm.expected.txt tmpcompare/3_20_hsb_adp.gmm.generated.txt
 
 echo "============== No output means all ok ======================="
 echo "============================================================="
