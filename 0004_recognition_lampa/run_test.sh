886e3c46a9c1c0d107bec1e179f834450e2efcf2
Daniel Zoba <daniel.sobe@sorben.com>
2021-08-19 10:51:21 +0200

    make test case seöf-checking

--- 0004_recognition_lampa/run_test.sh
+++ 0004_recognition_lampa/run_test.sh
@@ -5,16 +5,20 @@ export PATH=$PATH:~/dLabPro/bin.release/
 echo "====================================================="
 echo "=============== Generated results ==================="
 
+rm -f generated.txt
+touch generated.txt
 
 for i in $(ls -1 *.wav); do 
 	# echo "Recognizing $i"
-	recognizer -cfg recognizer.cfg $i -out res 2>/dev/null
-	recognizer -cfg recognizer.cfg $i -out dbg 2>/dev/null | grep "dbg: rec nad:"
+	recognizer -cfg recognizer.cfg $i -out res 2>/dev/null | tee -a generated.txt
+	recognizer -cfg recognizer.cfg $i -out dbg 2>/dev/null | grep "dbg: rec nad:" | tee -a generated.txt
 	# echo "--------------------------------------------"
 done
 
 echo "====================================================="
 echo "=============== Expected results ===================="
 
-cat expected.txt
+diff --brief expected.txt generated.txt
 
+echo "======== No output means all ok ====================="
+echo "====================================================="
