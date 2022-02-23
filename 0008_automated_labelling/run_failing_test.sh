06f7c4f50e9dd77f57ec80e1d07c2f1e3bb315ad
Daniel Zoba <daniel.sobe@sorben.com>
2022-02-23 16:13:09 +0100

    add alternative test that sometimes fails

---
+++ 0008_automated_labelling/run_failing_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export PATH=$PATH:~/dLabPro/bin.release/
+export UASR_HOME=$(pwd)/uasr
+
+echo "====================================================="
+echo "=============== Generated results ==================="
+
+# this directory shall always be empty each time the script is run
+rm -rf uasr-data/db-hsb-asr/common/lab
+mkdir -p uasr-data/db-hsb-asr/common/lab
+
+# clean log dir
+rm -rf uasr-data/db-hsb-asr/HSB-01/log
+mkdir -p uasr-data/db-hsb-asr/HSB-01/log
+
+dlabpro $HOME/UASR/scripts/dlabpro/HMM.xtp lab uasr-data/db-hsb-asr/HSB-01/info/label_fail.cfg
+
+echo "====================================================="
+echo "====== Compare with expected results ================"
+
+diff -Naur expected/ uasr-data/db-hsb-asr/common/lab/
+
+echo "======== Small differences maybe ok ================="
+echo "====================================================="
+
